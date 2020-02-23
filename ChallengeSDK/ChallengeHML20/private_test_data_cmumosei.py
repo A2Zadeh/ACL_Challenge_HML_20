5f000b8678936362314ecb51bbd457f77b158578
Amir Zadeh <abagherz@cs.cmu.edu>
2020-02-23 02:54:46 -0500

    Update private_test_data_cmumosei.py

--- ChallengeSDK/ChallengeHML20/private_test_data_cmumosei.py
+++ ChallengeSDK/ChallengeHML20/private_test_data_cmumosei.py
@@ -1,17 +1,3 @@
-print ("Challenge test set will be announced on February 15th 2020 - pull the SDK again on that day!")
-
-challenge20_data_private={}
-
-#the raw data for the challenge
-challenge20_data_private["raw"]={}
-
-#the highlevel data for the challenge
-challenge20_data_private["highlevel"]={}
-
-#the labels for the challenge
-challenge20_data_private["labels"]={}
-
-#extra data provided to users 
-challenge20_data_private["extra"]={}
-
+print ("Challenge test set released! It is a part of the released computational sequences (3837 ids).")
+print ("Align against the private test fold to get the data. Example is released.")
 
